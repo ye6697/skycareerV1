d52b7cddff9c26e2b59e5b8e3bbc3efbeab88c2c
MySupplieX <yusuf79_@live.de>
2026-02-06 23:45:16 +0000

    File changes

--- src/pages/Fleet.jsx
+++ src/pages/Fleet.jsx
@@ -308,10 +308,6 @@ export default function Fleet() {
       }
     },
     onSuccess: () => {
-      (async () => {
-        const aircraftList = await base44.entities.Aircraft.list('-created_date');
-        setFleetAircraft(aircraftList);
-      })();
       queryClient.invalidateQueries({ queryKey: ['company'] });
       setIsPurchaseDialogOpen(false);
       setSelectedAircraft(null);
