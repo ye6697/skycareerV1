762c1ce4c55c5ebee98434d5b6169c2318caafbc
MySupplieX <yusuf79_@live.de>
2026-02-06 23:44:57 +0000

    File changes

--- src/pages/Fleet.jsx
+++ src/pages/Fleet.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { base44 } from '@/api/base44Client';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { Button } from "@/components/ui/button";
