1e493c15fb8239c170c0346d765a1a01c87141b5
MySupplieX <yusuf79_@live.de>
2026-02-13 05:00:00 +0000

    File changes

--- src/components/flights/FlightMapIframe.jsx
+++ src/components/flights/FlightMapIframe.jsx
@@ -606,8 +606,8 @@ function update(d) {
   else if (!staticMode) { if(depPos) fp.push(depPos); if(curPos) fp.push(curPos); }
 
   if (layers.flown) map.removeLayer(layers.flown);
-  if (fp.length >= 2) {
-    layers.flown = L.polyline(fp, { color: currentViewMode==='arc' ? '#34d399' : '#3b82f6', weight:3.5, opacity:0.9 }).addTo(map);
+  if (fp.length >= 2 && currentViewMode !== 'arc') {
+    layers.flown = L.polyline(fp, { color: '#3b82f6', weight:3.5, opacity:0.9 }).addTo(map);
   }
 
   if (layers.dep) map.removeLayer(layers.dep);
